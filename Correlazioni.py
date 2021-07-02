f0deebae7531ffb4f1d3bb918743a9d77309cdc5
MatteoGaleone <69458316+MatteoGaleone@users.noreply.github.com>
2021-07-02 11:38:24 +0200

    Add files via upload

---
+++ Correlazioni.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import yfinance as yf
+
+
+# In[10]:
+
+
+def Programma():
+    while True:
+        N=int(input('Numero di asset da confrontare: '))
+        DF=CalcolaMatrice(N)
+        plt.figure(figsize=(10,8), dpi=150)
+        sns.heatmap(DF.corr(), cmap='RdYlGn', linecolor='black', linewidth=0.1, annot=True)
+        plt.show()
+        plt.clf()
+        while True:
+            risposta = str(input('Vuoi visualizzare una nuova matrice? (si/no): '))
+            if risposta in ('si', 'no'):
+                break
+            print('Input non valido.')
+        if risposta == 'si':
+            continue
+        else:
+            print('Ciao')
+            break
+
+
+# In[11]:
+
+
+def CalcolaMatrice(N):
+    DF=pd.DataFrame()
+    p=str(input('inserire i numero di periodi da considerare + y/mo/d, es: 5mo '))
+    for i in range(1, N+1) :
+            t=str(input("Inserire il ticker del prodotto: "))
+            Dataset= yf.Ticker(t)
+            DF[t]=Dataset.history(period=p)['Close']
+    return DF
+
+
+# In[8]:
+
+
+Programma()
+
+
+# In[ ]:
+
+
+2+1
+
+
+# In[ ]:
+
+
+
+
